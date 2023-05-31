a3223a8f5e77fa6e077c47e5ed56f907daa7c18d
bushidog <lamantinrtv@gmail.com>
2023-05-31 19:05:37 +0300

    upd

--- src/s21_smartcalc.c
+++ src/s21_smartcalc.c
@@ -1,23 +1,14 @@
 #include "s21_smartcalc.h"
-#include <string.h>
 
 int main() {
-  double result = MATH_FUNCTION_RESULT(tan, 0.8);
-  printf("tan(0.8) = %lf\n", result);
-  char buf[255] = "1 + sin(4/2) ^ 2";
-  // 3 4 2 * 1 5 - 2 ^ / +
+  char buf[255] = "sin(1) + cos(1)";
 
   Stack stack1;
   init(&stack1);
 
   fillStackDijkstra(&stack1, buf);
-  for (int i = stack1.top; i >= 0; i--) {
-    if (stack1.is_num[i] == 0) {
-      printf("%s\n", LONG_OPERANDS[(int)stack1.data[i]]);
-    } else {
-      printf("%lf\n", stack1.data[i]);
-    }
-  }
+  double result = pop(&stack1);
+  printf("%lf\n", result);
 
   return 0;
 }
@@ -35,18 +26,18 @@ void fillStackDijkstra(Stack *stack, const char *expression) {
         pop(&tmp);
         while (tmp.data[tmp.top] != 15.0) {
           push(stack, pop(&tmp), 0);
+          countValue(stack);
         }
         pop(&tmp);
-      } else if (tmp.top >= 1 &&
-                 (LONG_OPERANDS_PRIORITY[(int)tmp.data[tmp.top]] <=
+      } else if ((LONG_OPERANDS_PRIORITY[(int)tmp.data[tmp.top]] <=
                   LONG_OPERANDS_PRIORITY[(int)tmp.data[tmp.top - 1]]) &&
                  tmp.data[tmp.top] != 15.0) {
         int priority = LONG_OPERANDS_PRIORITY[(int)tmp.data[tmp.top]];
         pop(&tmp);
-        while (tmp.top >= 1 &&
-               (LONG_OPERANDS_PRIORITY[(int)tmp.data[tmp.top]] >= priority) &&
+        while ((LONG_OPERANDS_PRIORITY[(int)tmp.data[tmp.top]] >= priority) &&
                !isEmpty(&tmp)) {
           push(stack, pop(&tmp), 0);
+          countValue(stack);
         }
         push(&tmp, alt_names[ret], 0);
       }
@@ -63,5 +54,19 @@ void fillStackDijkstra(Stack *stack, const char *expression) {
   while (!isEmpty(&tmp)) {
     double num = pop(&tmp);
     push(stack, num, 0);
+    countValue(stack);
+  }
+}
+
+void countValue(Stack *stack) {
+  double alt_name_op = pop(stack);
+  if (0 <= alt_name_op && alt_name_op <= 8) {
+    double res =
+        MATH_FUNCTION_RESULT(trig_functions[(int)alt_name_op], pop(stack));
+    push(stack, res, 1);
+  } else {
+    double res =
+        countLOWop(LONG_OPERANDS[(int)alt_name_op], pop(stack), pop(stack));
+    push(stack, res, 1);
   }
 }
--- src/s21_smartcalc.h
+++ src/s21_smartcalc.h
@@ -1,5 +1,4 @@
 #include <ctype.h>
-#include <dlfcn.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,12 +7,12 @@
 #include "s21_stack.h"
 
 typedef double (*MathFunc)(double);
-MathFunc trig_functions[] = {cos, sin, tan, acos, asin, atan, sqrt, log};
+MathFunc trig_functions[] = {cos, sin, tan, acos, asin, atan, sqrt, log, log10};
 
 #define NUM_OF_L_OP 17
-const char *LONG_OPERANDS[] = {"cos",  "sin",  "mod", "tan", "acos", "asin",
-                               "atan", "sqrt", "ln",  "log", "+",    "-",
-                               "*",    "/",    "^",   "(",   ")"};
+const char *LONG_OPERANDS[] = {"cos",  "sin", "tan", "acos", "asin", "atan",
+                               "sqrt", "ln",  "log", "mod",  "+",    "-",
+                               "*",    "/",   "^",   "(",    ")"};
 const int LONG_OPERANDS_PRIORITY[] = {5, 5, 5, 5, 5, 5, 5, 5, 5,
                                       5, 2, 2, 3, 3, 4, 1, 1};
 const double alt_names[] = {0, 1,  2,  3,  4,  5,  6,  7, 8,
@@ -36,6 +35,28 @@ const double alt_names[] = {0, 1,  2,  3,  4,  5,  6,  7, 8,
     result;                                                                    \
   })
 
+#define countLOWop(operator, value1, value2)                                   \
+  ({                                                                           \
+    typeof(value1) _v1 = (value1);                                             \
+    typeof(value2) _v2 = (value2);                                             \
+    typeof(_v1) _result;                                                       \
+    if (!strncmp((operator), "+", 1)) {                                        \
+      _result = _v2 + _v1;                                                     \
+    } else if (!strncmp((operator), "-", 1)) {                                 \
+      _result = _v2 - _v1;                                                     \
+    } else if (!strncmp((operator), "*", 1)) {                                 \
+      _result = _v2 * _v1;                                                     \
+    } else if (!strncmp((operator), "/", 1)) {                                 \
+      _result = _v2 / _v1;                                                     \
+    } else if (!strncmp((operator), "^", 1)) {                                 \
+      _result = pow(_v2, _v1);                                                 \
+    } else if (!strncmp((operator), "mod", 3)) {                               \
+      _result = fmod(_v2, _v1);                                                \
+    }                                                                          \
+    _result;                                                                   \
+  })
+
 #define MATH_FUNCTION_RESULT(func, value) (func(value))
 
 void fillStackDijkstra(Stack *stack, const char *expression);
+void countValue(Stack *stack);
