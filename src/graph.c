9ff54920647590a35988251e942a9de01433655d
bushidog <lamantinrtv@gmail.com>
2023-06-12 00:15:15 +0300

    upd

--- src/graph.c
+++ src/graph.c
@@ -8,6 +8,127 @@
 GtkWidget *window;
 GtkWidget *expression_label;
 
+GtkWidget *credit_window;
+GtkWidget *credit_window2;
+GtkWidget *amount_entry;
+GtkWidget *rate_entry;
+GtkWidget *term_entry;
+GtkWidget *type_combo;
+GtkWidget *chart_area;
+GtkWidget *payment_label;
+GtkWidget *total_label;
+
+void calculate_credit(GtkWidget *widget, gpointer data) {
+  // Получение значений из полей
+  const gchar *amount_str = gtk_entry_get_text(GTK_ENTRY(amount_entry));
+  const gchar *rate_str = gtk_entry_get_text(GTK_ENTRY(rate_entry));
+  const gchar *term_str = gtk_entry_get_text(GTK_ENTRY(term_entry));
+  const gchar *type_str = gtk_combo_box_text_get_active_text(
+      GTK_COMBO_BOX_TEXT(type_combo));
+
+  // Преобразование строковых значений в числа
+  double amount = atof(amount_str);
+  double rate = atof(rate_str);
+  int term = atoi(term_str);
+
+  // Выполнение расчетов и отрисовка графика
+
+  // Размеры полей и графика
+  int field_width = 200;
+  int field_height = 25;
+  int chart_width = 200;
+  int chart_height = 100;
+
+  // Создание полей для отображения результатов
+  GtkWidget *amount_label = gtk_label_new(NULL);
+  GtkWidget *rate_label = gtk_label_new(NULL);
+  GtkWidget *term_label = gtk_label_new(NULL);
+  GtkWidget *type_label = gtk_label_new(NULL);
+
+  // Создание графика
+  GtkWidget *chart = gtk_drawing_area_new();
+  gtk_widget_set_size_request(chart, chart_width, chart_height);
+
+  // Создание меток для отображения ежемесячного платежа и общей суммы кредита
+  payment_label = gtk_label_new(NULL);
+  total_label = gtk_label_new(NULL);
+
+  credit_window2 = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(credit_window2), "Кредитный калькулятор");
+  // Расположение полей, графика и меток в окне
+  GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+  gtk_container_add(GTK_CONTAINER(credit_window2), box);
+
+  gtk_box_pack_start(GTK_BOX(box), amount_label, FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(box), rate_label, FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(box), term_label, FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(box), type_label, FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(box), chart, FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(box), payment_label, FALSE, FALSE, 0);
+  gtk_box_pack_start(GTK_BOX(box), total_label, FALSE, FALSE, 0);
+
+  // Отображение результатов
+  char result_str[100];
+
+  sprintf(result_str, "Сумма кредита: %.2f", amount);
+  gtk_label_set_text(GTK_LABEL(amount_label), result_str);
+
+  sprintf(result_str, "Ставка процента: %.2f", rate);
+  gtk_label_set_text(GTK_LABEL(rate_label), result_str);
+
+  sprintf(result_str, "Срок кредита: %d", term);
+  gtk_label_set_text(GTK_LABEL(term_label), result_str);
+
+  sprintf(result_str, "Тип кредита: %s", type_str);
+  gtk_label_set_text(GTK_LABEL(type_label), result_str);
+
+  gtk_widget_show_all(credit_window2);
+  // Очистка памяти и освобождение ресурсов
+  g_free((gpointer)type_str);
+}
+
+void button2_clicked(GtkWidget *widget, gpointer data) {
+  // Создание окна для кредитного калькулятора
+  credit_window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(credit_window), "Кредитный калькулятор");
+  
+  // Создание контейнера для кнопок
+  GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
+  gtk_container_add(GTK_CONTAINER(credit_window), box);
+
+  // Создание полей ввода и кнопки "Рассчитать"
+  GtkWidget *grid = gtk_grid_new();
+  gtk_box_pack_start(GTK_BOX(box), grid, FALSE, FALSE, 15);
+
+  GtkWidget *amount_label = gtk_label_new("Сумма кредита:");
+  GtkWidget *rate_label = gtk_label_new("Ставка процента:");
+  GtkWidget *term_label = gtk_label_new("Срок кредита:");
+  GtkWidget *type_label = gtk_label_new("Тип кредита:");
+
+  amount_entry = gtk_entry_new();
+  rate_entry = gtk_entry_new();
+  term_entry = gtk_entry_new();
+  type_combo = gtk_combo_box_text_new();
+
+  gtk_grid_attach(GTK_GRID(grid), amount_label, 0, 0, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), rate_label, 0, 1, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), term_label, 0, 2, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), type_label, 0, 3, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), amount_entry, 1, 0, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), rate_entry, 1, 1, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), term_entry, 1, 2, 1, 1);
+  gtk_grid_attach(GTK_GRID(grid), type_combo, 1, 3, 1, 1);
+
+  gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(type_combo), "Аннуитетный");
+  gtk_combo_box_text_append_text(GTK_COMBO_BOX_TEXT(type_combo), "Дифференцированный");
+
+  GtkWidget *calculate_button = gtk_button_new_with_label("Рассчитать");
+  g_signal_connect(G_OBJECT(calculate_button), "clicked", G_CALLBACK(calculate_credit), NULL);
+  gtk_box_pack_start(GTK_BOX(box), calculate_button, FALSE, FALSE, 0);
+
+  gtk_widget_show_all(credit_window);
+}
+
 gboolean draw_callback(GtkWidget *widget, cairo_t *cr, const char *expression) {
   cairo_set_source_rgb(cr, 1.0, 1.0, 1.0);
   cairo_paint(cr);
@@ -132,7 +253,7 @@ int main(int argc, char *argv[]) {
   gtk_init(&argc, &argv);
 
   window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-  gtk_window_set_title(GTK_WINDOW(window), "Введение в GTK");
+  gtk_window_set_title(GTK_WINDOW(window), "s21_smartcalc");
   g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit),
                    NULL);
 
@@ -160,6 +281,7 @@ int main(int argc, char *argv[]) {
 
   g_signal_connect(G_OBJECT(button1), "clicked", G_CALLBACK(button1_clicked),
                    NULL);
+  g_signal_connect(G_OBJECT(button2), "clicked", G_CALLBACK(button2_clicked), NULL);
 
   gtk_widget_show_all(window);
 
